856a8aa2a9de20209832ed927528b19ae6f8f6ee
GHKIM <kevink0@naver.com>
2021-07-20 20:26:13 +0900

    Initial commit

---
+++ src/main/java/com/cbu/springmentoringgoal/SpringMentoringGoalApplication.java
@@ -0,0 +1,13 @@
+package com.cbu.springmentoringgoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMentoringGoalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMentoringGoalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cbu/springmentoringgoal/SpringMentoringGoalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cbu.springmentoringgoal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMentoringGoalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
